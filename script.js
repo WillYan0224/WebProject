91abf4d13925187bdc8da1a4866d8bc64bae064d
WillYan <yanchung1998@gmail.com>
2025-05-20 20:00:06 +0900

    dark theme added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const menuButtons = document.querySelectorAll(".menu-button");
 const screenOverlay = document.querySelector(".screen-overlay");
+const themebutton = document.querySelector(".theme-button i")
+;
 
 menuButtons.forEach(button => {
   button.addEventListener("click", () => {
@@ -10,3 +12,21 @@ menuButtons.forEach(button => {
 screenOverlay.addEventListener("click", () => {
   document.body.classList.toggle("sidebar-hidden");
 });
+
+if(localStorage.getItem("darkMode") === "enabled")
+{
+  document.body.classList.add("dark-mode");
+  themebutton.classList.replace("uil-moon", "uil-sun");
+}
+else
+{
+  themebutton.classList.replace("uil-sun", "uil-moon");
+}
+
+themebutton.addEventListener("click", () => {
+  const isDarkMode = document.body.classList.toggle("dark-mode");
+  localStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled");
+  themebutton.classList.toggle("uil-sun", isDarkMode); 
+  themebutton.classList.toggle("uil-moon", !isDarkMode);
+});
+
